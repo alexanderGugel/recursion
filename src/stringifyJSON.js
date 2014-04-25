2da7868d6a6ed015b257d9762cd476b3d537399d
Alexander Gugel <alexander.gugel@gmail.com>
2014-04-25 13:15:03 +0200

    strinigfyJSON: first attempt

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,80 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var brain = {
+    null: 'null',
+    true: 'true',
+    false: 'false'
+  };
+  var results = [];
+  if (obj instanceof Array) {
+    for (var i = 0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+    }
+    return '[' + results.join(',') + ']';
+  } else if (obj === null || obj === true || obj === false) {
+    return brain[obj];
+  } else if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  } else if (typeof obj === 'object') {
+    for (var key in obj) {
+      results.push('"' + key + '":' + stringifyJSON(obj[key]) + '')
+    }
+    if (results.length === 1) {
+      results = results.join('},{');
+    }
+    return '{' + results + '}'
+  } else {
+    return obj;
+  }
 };
+
+console.log(stringifyJSON([
+  9,
+  null,
+  true,
+  false,
+  "Hello world",
+  [],
+  [8],
+  ["hi"],
+  [8, "hi"],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[],3,4]],
+  [[[["foo"]]]],
+  {},
+  {"a": "apple"},
+  {"foo": true, "bar": false, "baz": null},
+  {"boolean, true": true, "boolean, false": false, "null": null },
+  // basic nesting
+  {"a":{"b":"c"}},
+  {"a":["b", "c"]},
+  [{"a":"b"}, {"c":"d"}],
+  {"a":[],"c": {}, "b": true}
+]));
+
+
+console.log(JSON.stringify([
+  9,
+  null,
+  true,
+  false,
+  "Hello world",
+  [],
+  [8],
+  ["hi"],
+  [8, "hi"],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[],3,4]],
+  [[[["foo"]]]],
+  {},
+  {"a": "apple"},
+  {"foo": true, "bar": false, "baz": null},
+  {"boolean, true": true, "boolean, false": false, "null": null },
+  // basic nesting
+  {"a":{"b":"c"}},
+  {"a":["b", "c"]},
+  [{"a":"b"}, {"c":"d"}],
+  {"a":[],"c": {}, "b": true}
+]));
+
