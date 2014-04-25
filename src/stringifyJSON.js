f789604e1d045291837505ae524286303fb8511e
Alexander Gugel <alexander.gugel@gmail.com>
2014-04-25 13:26:14 +0200

    Make stringifyJSON work

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,26 +8,31 @@ var stringifyJSON = function(obj) {
     true: 'true',
     false: 'false'
   };
+  
   var results = [];
   if (obj instanceof Array) {
     for (var i = 0; i < obj.length; i++) {
-      results.push(stringifyJSON(obj[i]));
+      if (obj[i] !== undefined && typeof obj[i] !== 'function') {
+        results.push(stringifyJSON(obj[i]));        
+      }
     }
     return '[' + results.join(',') + ']';
   } else if (obj === null || obj === true || obj === false) {
     return brain[obj];
-  } else if (typeof obj === 'string'){
-    return '"' + obj + '"';
   } else if (typeof obj === 'object') {
     for (var key in obj) {
-      results.push('"' + key + '":' + stringifyJSON(obj[key]) + '')
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        results.push('"' + key + '":' + stringifyJSON(obj[key]) + '')        
+      }
     }
     if (results.length === 1) {
       results = results.join('},{');
     }
     return '{' + results + '}'
+  } else if (typeof obj === 'number') {
+    return obj.toString();
   } else {
-    return obj;
+    return '"' + obj + '"';
   }
 };
 
